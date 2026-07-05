052552607d782731490952f04ddac667c5474ab0
GULSHAN KUMAR <aayusharma2828@gmail.com>
2026-07-05 14:35:10 +0530

    Time: 0 ms (100%), Space: 33.4 MB (0%) - LeetHub

---
+++ 3978-unique-middle-element/3978-unique-middle-element.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isMiddleElementUnique(vector<int>& nums) {
+        int n = nums.size();
+
+        unordered_map<int , int> mp;
+        for(int x : nums){
+            mp[x]++;
+        }
+        if( mp[nums[n/2]] == 1 ) return true;
+        return false;
+    }
+};
