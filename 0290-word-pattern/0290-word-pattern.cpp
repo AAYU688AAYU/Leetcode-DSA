a8e453f08bf9f46321feb43b3cae43261ae8e439
GULSHAN KUMAR <aayusharma2828@gmail.com>
2026-07-15 11:39:52 +0530

    Time: 0 ms (100%), Space: 8.9 MB (12.65%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        vector<string> words;
+        string temp;
+        
+        stringstream ss(s);
+        while (ss >> temp) {
+            words.push_back(temp);
+        }
+        
+        if (pattern.size() != words.size()) return false;
+        
+        unordered_map<char, string> charToWord;
+        unordered_map<string, char> wordToChar;
+        
+        for (int i = 0; i < pattern.size(); i++) {
+            char c = pattern[i];
+            string w = words[i];
+            
+            if (charToWord.count(c)) {
+                if (charToWord[c] != w) return false;
+            } else {
+                charToWord[c] = w;
+            }
+            
+           
+            if (wordToChar.count(w)) {
+                if (wordToChar[w] != c) return false;
+            } else {
+                wordToChar[w] = c;
+            }
+        }
+        
+        return true;
+    }
+};
