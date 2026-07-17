0ec2b6adb4257efb8ffecf51e411ded39f583315
GULSHAN KUMAR <aayusharma2828@gmail.com>
2026-07-17 17:06:45 +0530

    Time: 0 ms (100%), Space: 11.1 MB (22.17%) - LeetHub

--- 2395-find-subarrays-with-equal-sum/2395-find-subarrays-with-equal-sum.cpp
+++ 2395-find-subarrays-with-equal-sum/2395-find-subarrays-with-equal-sum.cpp
@@ -4,14 +4,14 @@ public:
         int n = nums.size();
         int sum1;
 
-        unordered_map<int , int> mp;
+        set<int> mp;
 
         for(int i = 0;i<n - 1;i++){
             sum1 = nums[i] + nums[i+1];
             if(mp.count(sum1)){
                 return true;
             }
-            mp[sum1]++;
+            mp.insert(sum1);
         }
         return false;
 
